01d706b862fcd5805bd4699d097d6a0ad747417f
tamilCodingWizard <tamilcodingwizard@gmail.com>
2023-04-11 01:12:07 +0100

    video-07

--- Backend/controllers/taskController.js
+++ Backend/controllers/taskController.js
@@ -27,7 +27,7 @@ const getTasks = async (req, res) => {
 const getSingleTask = async (req, res) => {
   const { id } = req.params;
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ message: "Task Not Found" });
+    return res.status(404).json({ error: "Task Not Found" });
   }
   try {
     const singleTask = await taskModel.findById(id);
@@ -37,4 +37,49 @@ const getSingleTask = async (req, res) => {
   }
 };
 
-module.exports = { createTask, getTasks, getSingleTask };
+// To update a task - PATCH
+
+const updateTask = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Task Not Found" });
+  }
+
+  try {
+    const task = await taskModel.findByIdAndUpdate(
+      {
+        _id: id,
+      },
+      {
+        ...req.body,
+      }
+    );
+    res.status(200).json(task);
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
+};
+
+// Delete task - DELETE
+
+const deleteTask = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Task Not Found" });
+  }
+
+  try {
+    const task = await taskModel.findByIdAndDelete(id);
+    res.status(200).json(task);
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
+};
+
+module.exports = {
+  createTask,
+  getTasks,
+  getSingleTask,
+  updateTask,
+  deleteTask,
+};
--- Backend/routes/taskRoute.js
+++ Backend/routes/taskRoute.js
@@ -6,10 +6,14 @@ const {
   createTask,
   getTasks,
   getSingleTask,
+  updateTask,
+  deleteTask,
 } = require("../controllers/taskController");
 
 router.post("/", createTask);
 router.get("/", getTasks);
 router.get("/:id", getSingleTask);
+router.patch("/:id", updateTask);
+router.delete("/:id", deleteTask);
 
 module.exports = router;
